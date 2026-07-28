f2916cf108506cfb24db76381728eac17530453b
l <kbkbsantos@gmail.com>
2026-07-28 10:56:31 -0300

    Convertendo nomes somente  para letras maiúsculas e excluindo nomes com números

--- js/app.js
+++ js/app.js
@@ -1,25 +1,53 @@
+    // Cria um vetor (array) que armazenará todos os nomes digitados.
     let amigos = [];
 
-function adicionar () {
-    let amigo = document.getElementById('nome-amigo');
-    if(amigo.value == '') {
+    // Captura o texto digitado no campo,
+    // remove espaços extras e converte para letras maiúsculas.
+    function adicionar () {
+    let amigo = document.getElementById('nome-amigo').value.trim().toUpperCase();
+
+    // Verifica se o usuário deixou o campo vazio.
+    if (amigo === '') {
         alert('Digite o nome do amigo!');
         return;
     }
-    if(amigos.includes(amigo.value)){
-        alert('Nome ja adicionado!');
+
+
+    //  Impede a adição se houver qualquer número (0 a 9)
+    if (/\d/.test(amigo)) {
+        alert('O nome do amigo não pode conter números!');
         return;
     }
 
-    let lista = document.getElementById('lista-amigos');
-    amigos.push(amigo.value);
-    if(lista.textContent == '') {
-        lista.textContent = amigo.value;
-    }else{
-        lista.textContent = lista.textContent + ', ' + amigo.value;
+
+
+    // Verifica se o nome já foi cadastrado anteriormente.
+    if (amigos.includes(amigo)) {
+        alert('Nome já adicionado!');
+        return;
     }
-    amigo.value = '';
+
+    
+    // Localiza o elemento HTML onde será exibida a lista de amigos.
+    let lista = document.getElementById('lista-amigos');
+
+    // Adiciona o novo nome ao array.
+    amigos.push(amigo);
+
+    // Se for o primeiro nome, apenas exibe.
+    if(lista.textContent == '') {
+        lista.textContent = amigo;
+
+    // Caso já existam nomes, adiciona uma vírgula antes do novo.
+    }else{
+        lista.textContent = lista.textContent + ', ' + amigo;
+    }
+    
+    // Limpa o campo para facilitar o próximo cadastro.
+    document.getElementById('nome-amigo').value = '';
 }
+
+
 function sortear() {
     if(amigos.length < 4) {
         alert('Adicione pelo menos 4 amigos para realizar o sorteio!');
